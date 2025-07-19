6fe9ad25ee0aace77227ca5aa69a7131ca5587f8
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-19 18:23:51 -0300

    Aula 3: Keywords e tipos primitivos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
-
-    private final static String WELCOME_MESSAGE = "Olá, informe o seu nome";
-
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println(WELCOME_MESSAGE);
-        var name = scanner.next();
-        System.out.println("Olá, informe sua idade: ");
-        var age = scanner.nextInt();
-        System.out.printf("Olá %s sua idade é %s \n", name, age);
+        byte numberB = 1;
+        int numberI = 100;
+        double numberD = 1.0;
+        float numberF = 1.0f;
+        long numberL = 10L;
+        char character = 1;
+        var bool = true;
     }
 }
