fbf92bd108e80d8644303c99e45cf1ffac56f30e
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-19 18:08:22 -0300

    Aula 2: Padrões de desenvolvimento e conceitos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private final static String WELCOME_MESSAGE = "Olá, informe o seu nome";
+
     public static void main(String[] args) {
-        System.out.println("Hello world.");
-        String msg = "Programando em";
-        System.out.println(msg + args[0]);
-        System.out.println("Precisamos de uma " + args[1]);
+        var scanner = new Scanner(System.in);
+        System.out.println(WELCOME_MESSAGE);
+        var name = scanner.next();
+        System.out.println("Olá, informe sua idade: ");
+        var age = scanner.nextInt();
+        System.out.printf("Olá %s sua idade é %s \n", name, age);
     }
 }
