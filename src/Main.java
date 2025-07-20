0a48418660f3b5075b563dca4af1f4850fa2bed6
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-20 18:50:45 -0300

    Aula 4: Trabalhando com Operadores de Atribuição e Lógicos

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        byte numberB = 1;
-        int numberI = 100;
-        double numberD = 1.0;
-        float numberF = 1.0f;
-        long numberL = 10L;
-        char character = 1;
-        var bool = true;
+        System.out.printf("true  && true = %s \n", true && true);
+        System.out.printf("false && false = %s \n", false && false);
+        System.out.printf("true  && false = %s \n", true && false);
+        System.out.printf("false && true = %s \n", false && true);
+        System.out.println("======================================");
+        System.out.printf("true  || true = %s \n", true || true);
+        System.out.printf("false || false = %s \n", false || false);
+        System.out.printf("true  || false = %s \n", true || false);
+        System.out.printf("false || true = %s \n", false || true);
+        System.out.println("======================================");
+        System.out.printf("!true = %s \n", !true);
+        System.out.printf("!false = %s \n", !false);
+        System.out.println("======================================");
     }
 }
