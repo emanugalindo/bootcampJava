24a8a8f0fb9f8f18633c7e8ab340e596188595ef
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-21 10:14:22 -0300

    Aula 5 : Trabalhando com Operadores Aritméticos

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("true  && true = %s \n", true && true);
-        System.out.printf("false && false = %s \n", false && false);
-        System.out.printf("true  && false = %s \n", true && false);
-        System.out.printf("false && true = %s \n", false && true);
-        System.out.println("======================================");
-        System.out.printf("true  || true = %s \n", true || true);
-        System.out.printf("false || false = %s \n", false || false);
-        System.out.printf("true  || false = %s \n", true || false);
-        System.out.printf("false || true = %s \n", false || true);
-        System.out.println("======================================");
-        System.out.printf("!true = %s \n", !true);
-        System.out.printf("!false = %s \n", !false);
-        System.out.println("======================================");
+        var value = 50;
+        System.out.println(++value);
+        System.out.println(value);
     }
 }
