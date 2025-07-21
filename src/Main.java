ffa297ea193680446f3a5ca07b8821bf46da363e
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-21 14:31:41 -0300

    Aula 9: Estrutura de Repetição for

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println("Informe um número de 1 até 7");
-        var option = scanner.nextInt();
-        var message = switch (option){
-            case 1,7 -> {
-                var day = option == 1 ? "Domingo" : "Sábado";
-                 yield String.format("Hoje é %s, fim de semana uhuull \\o/\n", day);
-            }
-            case 2 -> "Segunda";
-            case 3 -> "Terça";
-            case 4 -> "Quarta";
-            case 5 -> "Quinta";
-            case 6 -> "Sexta";
-            default -> "Opção inválida";
-        };
-        System.out.println(message);
+        for (var i = 0; i < args.length; i++) {
+            System.out.println((i + 1) + " - " + args[i]);
+        }
     }
 }
