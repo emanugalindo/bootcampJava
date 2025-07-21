4bf327f9b7f442e94e568a79cba76a289fc38511
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-21 14:15:06 -0300

    Aula 8: Estrutura Condicional switch case

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
-        System.out.println("Informe seu nome:");
-        var name = scanner.next();
-        System.out.println("Informe sua idade:");
-        var age = scanner.nextInt();
-        System.out.println("Você é emancipado? (s/n)");
-        var isEmancipated = scanner.next().equalsIgnoreCase("s");
-
-        var canDrive = (age >= 18) || (age >= 16 && isEmancipated);
-        var message = canDrive ?
-                name + ", você pode dirigir\n" :
-                name +", você não pode dirigir\n";
-
+        System.out.println("Informe um número de 1 até 7");
+        var option = scanner.nextInt();
+        var message = switch (option){
+            case 1,7 -> {
+                var day = option == 1 ? "Domingo" : "Sábado";
+                 yield String.format("Hoje é %s, fim de semana uhuull \\o/\n", day);
+            }
+            case 2 -> "Segunda";
+            case 3 -> "Terça";
+            case 4 -> "Quarta";
+            case 5 -> "Quinta";
+            case 6 -> "Sexta";
+            default -> "Opção inválida";
+        };
         System.out.println(message);
-        System.out.println("Fim da execução");
     }
 }
