d6f613554817a4228f8fb2097ec022cb73f4be23
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-21 18:11:04 -0300

    Aula 10: Estruturas de repetição while e do while

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (var i = 0; i < args.length; i++) {
-            System.out.println((i + 1) + " - " + args[i]);
+        var i = 0;
+        while (args.length > i){
+            System.out.println(args[i]);
+            i++;
         }
+
+        i = 0;
+
+        System.out.println("====================");
+        do {
+            System.out.println(args[i]);
+            i++;
+        }while (args.length > i);
     }
 }
