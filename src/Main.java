b0b413e45d3e67d95752f4ea4f8bf015f0008fee
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-21 10:56:04 -0300

    Aula 6: Trabalhando com Operadores Bitwise (Bit-a-Bit)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var value = 50;
-        System.out.println(++value);
-        System.out.println(value);
+        // Operadores Bitwise(Bit-a-Bit)
+        var value1 = -12;
+        var binary1 = Integer.toBinaryString(value1);
+        System.out.printf("Primeiro número da operação %s (representação binária %s)\n", value1, binary1);
+        var value2 = 2;
+        //var binary2 = Integer.toBinaryString(value2);
+        System.out.printf("Segundo número da operação %s\n", value2);
+        var result =  value1 >>> value2;
+        var binaryResult = Integer.toBinaryString(result);
+        System.out.printf("%s >>> %s = %s (representação binária %s)\n", value1, value2, result, binaryResult);
     }
 }
