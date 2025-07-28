4bb8adfb179125222d15d3be7db3f53f074bcd9b
emanugalindo <emanuel.galindo410@gmail.com>
2025-07-28 12:19:35 -0300

    Aula 13: Introdução a Herança e Polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        var person = new Person("João");
-        System.out.println(person.getInfo());
-        System.out.println(person.name());
+        Employee manager = new Manager();
+
+        manager.setName("João");
+//        manager.setLogin("joao");
+//        manager.setPassword("123456");
+
+        System.out.println(manager.getName());
+//        System.out.println(manager.getLogin());
+//        System.out.println(manager.getPassword());
     }
 }
